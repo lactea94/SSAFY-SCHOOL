c0eb8494cf6191e445dd31b682b3139a282cdfc7
dannyp0930 <dannyp0930@gmail.com>
2022-04-19 14:33:43 +0900

    FEAT: Home

---
+++ frontend/src/Components/Articles/Articles.js
@@ -0,0 +1,5 @@
+export function Articles() {
+  return (
+    <div>Articles</div>
+  )
+};
--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,5 +1,32 @@
+import "./Home.css";
+
 export function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <header>
+        <h1>
+          SSAFY SCHOOL
+        </h1>
+        <h4>
+          메타버스에서 만나는 광주 캠퍼스
+        </h4>
+      </header>
+      <content>
+        <div>
+          프로젝트 소개
+        </div>
+        <div>
+          기능 소개
+        </div>
+      </content>
+      <footer>
+        <p>
+          © 삼성청년SW아카데미 6기 광주캠퍼스 2반 장자도에서 하룻밤 💋
+        </p>
+        <p>
+          김동유 | 김병완 | 김현송 | 박상훈 | 박주윤 | 유병재
+        </p>
+      </footer>
+    </div>
   )
-}
+};
