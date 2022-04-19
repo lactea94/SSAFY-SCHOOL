1024c89d522ccfc6b9cfa9bce0bda23f991df3a3
dannyp0930 <dannyp0930@gmail.com>
2022-04-19 17:23:39 +0900

    FEAT: articles

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -6,6 +6,8 @@ import Articles from "../Page/Articles";
 import Login from "../Page/Login";
 import Signup from "../Page/Signup";
 import PageNotFound from "../Page/404";
+import Notice from "../Components/Articles/Notice";
+import TestBug from "../Components/Articles/TestBug";
 
 function App() {
   return (
@@ -13,7 +15,11 @@ function App() {
       <Nav/>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="articles" element={<Articles />}></Route>
+        <Route path="articles" element={<Articles />}>
+          <Route index element={<Notice />}></Route>
+          <Route path="notice" element={<Notice />}></Route>
+          <Route path="testbug" element={<TestBug />}></Route>
+        </Route>
         <Route path="profile" element={<Home />}></Route>
         <Route path="login" element={<Login />}></Route>
         <Route path="logout" element={<Home />}></Route>
--- frontend/src/Page/Articles.js
+++ frontend/src/Page/Articles.js
@@ -1,7 +1,12 @@
+import { NavLink, Outlet } from "react-router-dom";
+
 export default function Articles() {
   return (
     <div>
-      aldsjfklajslfkdsjalk
+      <h1>게시판</h1>
+      <NavLink to="notice">공지사항</NavLink>
+      <NavLink to="testbug">오류제보</NavLink>
+      <Outlet />
     </div>
   )
 };
