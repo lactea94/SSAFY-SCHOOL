81fd420dd5583a929adb91a7525888221e4d4375
김동유 <ehddb2252@gmail.com>
2022-04-19 15:01:07 +0900

    FEAT: create 404 page

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Nav from "../Common/Nav"
-import { Home } from "../Page/Home";
+import Home from "../Page/Home";
+import PageNotFound from "../Page/404";
 
 function App() {
   return (
@@ -8,6 +9,12 @@ function App() {
       <Nav/>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/articles" element={<Home />}></Route>
+        <Route path="/profile" element={<Home />}></Route>
+        <Route path="/login" element={<Home />}></Route>
+        <Route path="/logout" element={<Home />}></Route>
+        <Route path="/signup" element={<Home />}></Route>
+        <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
     </div>
   );
