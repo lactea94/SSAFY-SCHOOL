734bfa6c59c7c5163b2fa42bf3ce27c612d03977
김동유 <ehddb2252@gmail.com>
2022-04-19 13:30:53 +0900

    chore: 로고 추가

--- frontend/src/Nav.js
+++ frontend/src/Nav.js
@@ -2,7 +2,8 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function Nav() {
   return (
-    <div>
+    <div height="50px">
+      <img src="./image/logo.png" style={{display: "block",width:"auto", height:"100%"}}></img>
     </div>
   )
 }
