0369c4f8758d246c9ada442934215573c1b97c25
dannyp0930 <dannyp0930@gmail.com>
2022-04-19 17:29:44 +0900

    FEAT: articles

--- frontend/src/Page/Articles.js
+++ frontend/src/Page/Articles.js
@@ -4,8 +4,10 @@ export default function Articles() {
   return (
     <div>
       <h1>게시판</h1>
-      <NavLink to="notice">공지사항</NavLink>
-      <NavLink to="testbug">오류제보</NavLink>
+      <nav>
+        <NavLink to="notice">공지사항</NavLink>
+        <NavLink to="testbug">오류제보</NavLink>
+      </nav>
       <Outlet />
     </div>
   )
