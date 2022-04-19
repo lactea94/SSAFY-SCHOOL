82af868a032b017788f6e8743e6c9a365cb90eab
dannyp0930 <dannyp0930@gmail.com>
2022-04-19 13:28:29 +0900

    FEAT: router

---
+++ frontend/src/App/App.js
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Nav from "../Common/Nav"
+import { Home } from "../Components/Home/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
