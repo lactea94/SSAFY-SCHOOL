4618fa84ec1fd397bc7e4fdc60e3d184b18da586
김동유 <ehddb2252@gmail.com>
2022-04-19 13:09:41 +0900

    FEAT : create Nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
+import Nav from "./Nav"
+
 function App() {
   return (
     <div className="App">
+      <Nav/>
       <h1>Hi</h1>
     </div>
   );
---
+++ frontend/src/Nav.js
@@ -0,0 +1,10 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+function Nav() {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default Nav;
