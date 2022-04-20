ce788cd875d8afdaaa792d929ff6c41df1dba5b5
dannyp0930 <dannyp0930@gmail.com>
2022-04-20 14:51:22 +0900

    STYLE: articles

--- frontend/src/Components/Articles/Notice.js
+++ frontend/src/Components/Articles/Notice.js
@@ -17,6 +17,22 @@ export default function Notice() {
     { id: 10, title: "공지11", content: "공지내용11", createDate: "2022-04-09 15:30:30", updatedDate: "2022-04-10 15:30:30" },
     { id: 11, title: "공지12", content: "공지내용12", createDate: "2022-04-08 15:30:30", updatedDate: "2022-04-09 15:30:30" },
     { id: 12, title: "공지13", content: "공지내용13", createDate: "2022-04-07 15:30:30", updatedDate: "2022-04-08 15:30:30" },
+    { id: 13, title: "공지14", content: "공지내용14", createDate: "2022-04-14 15:30:30", updatedDate: "2022-04-15 15:30:30" },
+    { id: 14, title: "공지15", content: "공지내용15", createDate: "2022-04-13 15:30:30", updatedDate: "2022-04-14 15:30:30" },
+    { id: 15, title: "공지16", content: "공지내용16", createDate: "2022-04-12 15:30:30", updatedDate: "2022-04-13 15:30:30" },
+    { id: 16, title: "공지17", content: "공지내용17", createDate: "2022-04-11 15:30:30", updatedDate: "2022-04-12 15:30:30" },
+    { id: 17, title: "공지18", content: "공지내용18", createDate: "2022-04-10 15:30:30", updatedDate: "2022-04-11 15:30:30" },
+    { id: 18, title: "공지19", content: "공지내용19", createDate: "2022-04-09 15:30:30", updatedDate: "2022-04-10 15:30:30" },
+    { id: 19, title: "공지20", content: "공지내용20", createDate: "2022-04-08 15:30:30", updatedDate: "2022-04-09 15:30:30" },
+    { id: 20, title: "공지21", content: "공지내용21", createDate: "2022-04-07 15:30:30", updatedDate: "2022-04-08 15:30:30" },
+    { id: 21, title: "공지22", content: "공지내용22", createDate: "2022-04-14 15:30:30", updatedDate: "2022-04-15 15:30:30" },
+    { id: 22, title: "공지23", content: "공지내용23", createDate: "2022-04-13 15:30:30", updatedDate: "2022-04-14 15:30:30" },
+    { id: 23, title: "공지24", content: "공지내용24", createDate: "2022-04-12 15:30:30", updatedDate: "2022-04-13 15:30:30" },
+    { id: 24, title: "공지25", content: "공지내용25", createDate: "2022-04-11 15:30:30", updatedDate: "2022-04-12 15:30:30" },
+    { id: 25, title: "공지26", content: "공지내용26", createDate: "2022-04-10 15:30:30", updatedDate: "2022-04-11 15:30:30" },
+    { id: 26, title: "공지27", content: "공지내용27", createDate: "2022-04-09 15:30:30", updatedDate: "2022-04-10 15:30:30" },
+    { id: 27, title: "공지28", content: "공지내용28", createDate: "2022-04-08 15:30:30", updatedDate: "2022-04-09 15:30:30" },
+    { id: 28, title: "공지29", content: "공지내용29", createDate: "2022-04-07 15:30:30", updatedDate: "2022-04-08 15:30:30" },
   ];
 
   const [limit, setLimit] = useState(10);
