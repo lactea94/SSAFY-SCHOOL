d8ca1b1c8205f5ed2536fb45a86e942aa48e419a
dannyp0930 <dannyp0930@gmail.com>
2022-04-20 23:42:17 +0900

    FIX: nav logo

--- frontend/src/Common/Nav.js
+++ frontend/src/Common/Nav.js
@@ -6,10 +6,10 @@ function Nav() {
       <div style={{position: "fixed", height:"50px", width: "100%", minWidth: "740px", backgroundColor: "white", display: "flex", justifyContent: "space-between", borderTop: "0px", borderLeft: "0px", borderRight: "0px", borderBottom: "1px", borderStyle: "solid"}}>
         <div style={{display: "inline-block",height:"100%"}}>
           <Link to="/">
-            <img src="./image/logo.png" style={{height:"100%"}}></img>
+            <img src="/image/logo.png" style={{height:"100%"}} alt="logo"></img>
           </Link>
         </div>
-        <div style={{display: "inline-block",height:"100%", fontSize: "20px", display: "flex", alignItems: "center"}}>
+        <div style={{display: "flex", height:"100%", fontSize: "20px", alignItems: "center"}}>
           <Link to="articles/notice" style={{textDecoration: "none", color: "black", fontWeight: "bold", margin: "0 1rem"}}>
             게시판
           </Link>
