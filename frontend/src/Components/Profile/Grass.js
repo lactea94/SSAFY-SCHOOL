d9062c78024c275df4a864d8093a3b9f4019a462
김동유 <ehddb2252@gmail.com>
2022-04-21 11:21:04 +0900

    UPDATE: 잔디 색 수정

--- frontend/src/Components/Profile/Grass.js
+++ frontend/src/Components/Profile/Grass.js
@@ -10,8 +10,8 @@ export default function Grass() {
 
   useEffect(()=>{
     //입퇴실 정보 받아오기
-    setIndata(["2022-03-21","2022-03-22","2022-03-23","2022-03-24"]);
-    setOutdata(["2022-03-21","2022-03-22","2022-03-23","2022-03-25"]);
+    setIndata(["2022-03-21","2022-03-22","2022-03-23","2022-03-24", "2022-04-19", "2022-04-18", "2022-04-21"]);
+    setOutdata(["2022-03-21","2022-03-22","2022-03-23","2022-03-25", "2022-04-19", "2022-04-20", "2022-04-21"]);
   },[])
 
   useEffect(()=>{
@@ -116,8 +116,15 @@ export default function Grass() {
       let nowdate = new Date().getTime()
       let breakidx = Math.floor(
         (nowdate - startdate)/(1000*3600*24)
-      )
-
+        )
+      const todayInfo = new Date();
+      const today = {
+        year: todayInfo.getFullYear(), 
+        month: todayInfo.getMonth() + 1, 
+        date: todayInfo.getDate()
+      }
+      const monthStartIdx = breakidx - today.date + 1
+      const monthEndIdx = monthStartIdx + (new Date(today.year, today.month, 0)).getDate() - 1
       //잔디 포문으로 생성
       for(var i in grassdata){
         //토요일 넘어가면 일요일로 초기화
@@ -128,40 +135,84 @@ export default function Grass() {
         let grass = document.createElement("div");
         grass.id=i
         grass.style.cssText = `
-        position:absolute;
-        width:19px; 
-        height:19px;
-        background:#f3f3f3;
-        border-radius:30%;
-        margin:1px;
-        left:${leftposition + 5}px;
-        top:${(cnt*22) + 5}px;
+          position:absolute;
+          width:19px; 
+          height:19px;
+          background:#f3f3f3;
+          border-radius:30%;
+          margin:1px;
+          left:${leftposition + 5}px;
+          top:${(cnt*22) + 5}px;
         `
-
-        if (i==breakidx){
-          grass.style.border='1px solid tomato'
-        }
-
-        if (i>breakidx) {
-          container.appendChild(grass)
-          cnt += 1
-          continue
-        }
-
-        if(cnt==0 || cnt==6){
-          grass.style.backgroundColor='#bfe4ff'
-        }
-
-        if(grassdata[i] == 0b011){
-          grass.style.backgroundColor='#a1e4ac'
-        }
-
-        if(grassdata[i] == 0b001){
-          grass.style.backgroundColor='#ffd29b'
-        }
-
-        if(grassdata[i] >= 0b100){
-          grass.style.backgroundColor='#bfe4ff'
+        if(i >= monthStartIdx && i <= monthEndIdx) {
+          grass.style.backgroundColor='#dddfe0'
+          if (i==breakidx){
+            // let blinkEffect = `
+            //   border: 2px solid tomato;
+            //   left:${leftposition + 3}px;
+            //   top:${(cnt*22) + 3}px;
+            // `
+            // grass.style.animation = `${blinkEffect} 1s step-end infinite`
+            grass.style.border='2px solid tomato'
+            grass.style.left=`${leftposition + 3}px`
+            grass.style.top=`${(cnt*22) + 3}px`
+          }
+  
+          if (i>breakidx) {
+            container.appendChild(grass)
+            cnt += 1
+            continue
+          }
+          
+          if(grassdata[i] == 0b011){
+            grass.style.backgroundColor='#78cb94'
+          }
+  
+          if(grassdata[i] == 0b001){
+            grass.style.backgroundColor='#febb6c'
+          }
+          if(grassdata[i] == 0b010){
+            grass.style.backgroundColor='#febb6c'
+          }
+          
+          if(cnt==0 || cnt==6){
+            grass.style.backgroundColor='#80caff'
+          }
+          if(grassdata[i] >= 0b100){
+            grass.style.backgroundColor='#80caff'
+          }
+        }else {
+          if (i==breakidx){
+            grass.style.backgroundColor='#dddfe0'
+            grass.style.border='2px solid tomato'
+            grass.style.left=`${leftposition + 3}px`
+            grass.style.top=`${(cnt*22) + 3}px`
+          }
+  
+          if (i>breakidx) {
+            container.appendChild(grass)
+            cnt += 1
+            continue
+          }
+  
+          
+          if(grassdata[i] == 0b011){
+            grass.style.backgroundColor='#a1e4ac'
+          }
+          
+          if(grassdata[i] == 0b001){
+            grass.style.backgroundColor='#ffd29b'
+          }
+          if(grassdata[i] == 0b010){
+            grass.style.backgroundColor='#ffd29b'
+          }
+          
+          if(cnt==0 || cnt==6){
+            grass.style.backgroundColor='#bfe4ff'
+          }
+          if(grassdata[i] >= 0b100){
+            grass.style.backgroundColor='#bfe4ff'
+          }
         }
 
         container.appendChild(grass)
