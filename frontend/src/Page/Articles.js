a47c411ac5e1378fdd5dd7f9b13b86b658b494b3
dannyp0930 <dannyp0930@gmail.com>
2022-04-21 11:56:37 +0900

    STYLE: articles

--- frontend/src/Page/Articles.js
+++ frontend/src/Page/Articles.js
@@ -1,45 +1,21 @@
 import { NavLink, Outlet } from "react-router-dom";
 
 export default function Articles() {
-  const NavStyle = {
-    display: "grid",
-    gridTemplateColumns: "1fr 1fr",
-    margin: "3rem auto 2rem",
-    width: "50%",
-  };
 
   const NavLinkStyle = ({ isActive }) => ({
-    margin: "1rem",
-    padding: "0.5rem",
-    border: isActive ? "none" : "1px solid #3396F4",
-    borderRadius: "0.3rem",
-    backgroundColor: isActive ? "#3396F4" : "white",
-    fontSize: "1rem",
-    textDecoration: "none",
-    color: isActive ? "white" : "#3396F4",
+    borderTop: isActive ? "1px solid gray" : "none",
+    borderLeft: isActive ? "1px solid gray" : "none",
+    borderRight: isActive ? "1px solid gray" : "none",
+    borderBottom: isActive ? "none" : "1px solid gray",
+    color: isActive && "black",
   });
 
   return (
-    <div style={{textAlign: "center"}}>
-      <nav style={NavStyle}>
-        <div
-          style={{
-            display: "flex",
-            fontSize: "2.5rem",
-            alignItems: "center",
-          }}
-        >
-          게시판
-        </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "end",
-          }}
-        >
-          <NavLink style={NavLinkStyle} to="notice">공지 사항</NavLink>
-          <NavLink style={NavLinkStyle} to="testbug">오류 제보</NavLink>
-        </div>
+    <div className="article">
+      <nav className="article-nav">
+        <div className="nav-title">게시판</div>
+        <NavLink className="nav-link" style={NavLinkStyle} to="notice">공지 사항</NavLink>
+        <NavLink className="nav-link" style={NavLinkStyle} to="testbug">오류 제보</NavLink>
       </nav>
       <Outlet />
     </div>
