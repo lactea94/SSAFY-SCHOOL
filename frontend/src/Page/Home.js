37fe3cba3dafb20bf70726fcb978041ea20ec5b3
dannyp0930 <dannyp0930@gmail.com>
2022-04-21 15:45:34 +0900

    STYLE: Home

--- frontend/src/Page/Home.js
+++ frontend/src/Page/Home.js
@@ -1,5 +1,11 @@
 import "./Home.css";
 import ssafyImg from "../asset/image/ssafy.jpg";
+import metaverse_logo from "../asset/image/metaverse_logo.png";
+import remote_logo from "../asset/image/remote_logo.png";
+import mileage_logo from "../asset/image/mileage_logo.png";
+import metaverse_1 from "../asset/image/metaverse_1.png";
+import remote_1 from "../asset/image/remote_1.png";
+import mileage_1 from "../asset/image/mileage_1.png";
 
 export default function Home() {
   return (
@@ -28,45 +34,38 @@ export default function Home() {
           </div>
           <img src={ssafyImg} alt="ssafy"/>
         </div>
-        <div className="features">
-          <div>
+        <div className="left-feature">
+          <div className="feature-card">
             <img
-              src="/image/metaverse.png"
-              alt="metaverse"
-              style={{
-                width: "100%"
-              }}
+              src={metaverse_logo}
+              alt="metaverse_logo"
             />
             <h2>메타버스</h2>
             <p>메타버스로 즐기는 즐거운 싸피 생활!</p>
           </div>
-          <div>
+          <img className="feature-img" src={metaverse_1} alt="metaverse_1"></img>
+        </div>
+        <div className="right-feature">
+          <img className="feature-img" src={remote_1} alt="remote_1"></img>
+          <div className="feature-card">
             <img
-              src="/image/education.png"
-              alt="education"
-              style={{
-                width: "100%"
-              }}
+              src={remote_logo}
+              alt="remote_logo"
             />
             <h2>원격수업</h2>
             <p>원격으로 수업을 들을 수 있어요!</p>
           </div>
-          <div>
+        </div>
+        <div className="left-feature">
+          <div className="feature-card">
             <img
-              src="/image/shopping.png"
-              alt="shopping"
-              style={{
-                width: "100%"
-              }}
+              src={mileage_logo}
+              alt="mileage_logo"
             />
             <h2>마일리지</h2>
             <p>마일리지를 쌓아 나만의 캐릭터를 꾸며봐요!</p>
           </div>
-        </div>
-        <div className="function">
-          <div>
-            기능 소개
-          </div>
+          <img className="feature-img" src={mileage_1} alt="mileage_1"></img>
         </div>
       </main>
     </div>
