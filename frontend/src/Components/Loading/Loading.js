ef8c69d875d7a435de54712c557ed6ff74842eb3
dannyp0930 <dannyp0930@gmail.com>
2022-04-22 10:07:38 +0900

    FEAT: Loading

---
+++ frontend/src/Components/Loading/Loading.js
@@ -0,0 +1,18 @@
+export default function Loading() {
+  return (
+    <div
+      style={{
+        display: "flex",
+      }}
+    >
+      <img
+        src="/image/loading.gif"
+        alt="loading"
+        style={{
+          width: "80%",
+          margin: " 5rem auto",
+        }}
+      />
+    </div>
+  )
+}
