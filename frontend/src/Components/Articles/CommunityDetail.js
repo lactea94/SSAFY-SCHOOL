2db2b5e9b0342cacb808bff9c9c50f5f40e97f89
dannyp0930 <dannyp0930@gmail.com>
2022-04-26 15:26:10 +0900

    STYLE: CommunityDetail

--- frontend/src/Components/Articles/CommunityDetail.js
+++ frontend/src/Components/Articles/CommunityDetail.js
@@ -1,29 +1,20 @@
 import { useLocation } from "react-router-dom";
 import DateFormat from "../../Utils/DateFormat";
+import "./css/CommunityDetail.css"
 
 export default function CommunityDetail() {
   const { state } = useLocation();
-  const comments = [
-    { id: 0, content: "댓글1", createdDate: "2022-04-19 16:10:00", updatedDate: "2022-04-20 15:30:30" },
-    { id: 1, content: "댓글2", createdDate: "2022-04-18 15:31:00", updatedDate: "2022-04-20 11:30:00" },
-    { id: 2, content: "댓글3", createdDate: "2022-04-17 15:30:00", updatedDate: "2022-04-18 15:30:30" },
-    { id: 3, content: "댓글4", createdDate: "2022-04-16 15:30:00", updatedDate: "2022-04-17 15:30:30" },
-    { id: 4, content: "댓글5", createdDate: "2022-04-15 15:30:00", updatedDate: "2022-04-16 15:30:30" },
-  ]
-
-  function Comments() {
-    return (
-      comments.map((comment) => {
-        return(
-          <div
-            key={comment.id}
-          >
-            {comment.content}
-          </div>
-        )
-      })
-    )
+  const user = {
+    id: 0,
   }
+
+  const comments = [
+    { id: 0, userId: 0, content: "댓글1", createdDate: "2022-04-19 16:10:00", updatedDate: "2022-04-20 15:30:30" },
+    { id: 1, userId: 1, content: "댓글2", createdDate: "2022-04-18 15:31:00", updatedDate: "2022-04-20 11:30:00" },
+    { id: 2, userId: 3, content: "댓글3", createdDate: "2022-04-17 15:30:00", updatedDate: "2022-04-18 15:30:30" },
+    { id: 3, userId: 2, content: "댓글4", createdDate: "2022-04-16 15:30:00", updatedDate: "2022-04-17 15:30:30" },
+    { id: 4, userId: 1, content: "댓글5", createdDate: "2022-04-15 15:30:00", updatedDate: "2022-04-16 15:30:30" },
+  ]
 
   return (
     <div className="notice-container">
@@ -34,7 +25,37 @@ export default function CommunityDetail() {
       </div>
       <div className="notice-content">{state.content}</div>
       <div className="comments-container">
-        {Comments()}
+        <div className="comments-header">
+          댓글 {comments.length}
+        </div>
+        {comments.map((comment) => {
+          return (
+            <div
+              className="comment"
+              key={comment.id}
+            >
+              <div>
+                {comment.userId}
+              </div>
+              <div>
+                {comment.content}
+              </div>
+              <div>
+                {DateFormat(comment.createdDate)}
+              </div>
+              <div>
+                {DateFormat(comment.updatedDate)}
+              </div>
+              { user.id === comment.userId ? (
+                <div>
+                  삭제
+                </div>
+              ) : (
+                <div></div>
+              )}
+            </div>
+          )
+        })}
       </div>
     </div>
   )
