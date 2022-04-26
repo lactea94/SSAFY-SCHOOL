3a4d8539eccf85a47a30a4dd84e752087a7b8882
dannyp0930 <dannyp0930@gmail.com>
2022-04-26 17:18:22 +0900

    FEAT: CommunityCreate

--- frontend/src/Components/Articles/CommunityCreate.js
+++ frontend/src/Components/Articles/CommunityCreate.js
@@ -1,5 +1,34 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+import "./css/CommunityCreate.css"
+
 export default function CommunityCreate() {
+  const [ title, setTitle ] = useState();
+  const [ content, setContent ] = useState();
+  const navigate = useNavigate();
+
+  function handleClick() {
+    console.log(title);
+    console.log(content);
+    navigate('/articles/community')
+  }
+
   return (
-    <div>CommunityCreate</div>
+    <div className="create-form">
+      <div className="create-label">제목</div>
+      <input
+        className="create-title"
+        name="title"
+        onChange={(e) => {setTitle(e.target.value)}}
+      />
+      <div className="create-label">내용</div>
+      <textarea
+        className="create-content"
+        name="content"
+        rows={20}
+        onChange={(e) => {setContent(e.target.value)}}
+      />
+      <div className="create-button" onClick={handleClick}>작성</div>
+    </div>
   )
 };
