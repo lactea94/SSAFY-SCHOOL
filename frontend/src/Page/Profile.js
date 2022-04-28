2eefc9b6bf7a1ec5bf94ae1a616e9624d2a9fe6d
dannyp0930 <dannyp0930@gmail.com>
2022-04-28 16:14:28 +0900

    STYLE: Profile

--- frontend/src/Page/Profile.js
+++ frontend/src/Page/Profile.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import Maininfo from '../Components/Profile/Maininfo';
-import Mileage from '../Components/Profile/Mileage';
-import Attendance from '../Components/Profile/Attendance';
 import './css/Profile.css';
 import { useLocation } from 'react-router-dom';
+import Grass from '../Components/Profile/Grass';
 
 export default function Profile() {
   const { state } = useLocation();
   return (
-    <>
-      <div className='main'>
-        <div className='mainInfo'>
-          <Maininfo user={state.user}/>
-          <Mileage user={state.user}/>
-        </div>
-        <div className='additionalInfo'>
-          <Attendance/>
-        </div>
-      </div>
-    </>
+    <div className='main'>
+      <Maininfo user={state.user}/>
+      <Grass/>
+    </div>
   )
 };
