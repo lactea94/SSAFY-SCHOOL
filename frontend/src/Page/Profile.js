4e51ba8b2bf775c017846ba610feb741263b20b2
dannyp0930 <dannyp0930@gmail.com>
2022-04-30 23:38:22 +0900

    STYLE: Profile

--- frontend/src/Page/Profile.js
+++ frontend/src/Page/Profile.js
@@ -1,6 +1,7 @@
 import './css/Profile.css';
 import { Outlet, useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { FaUserEdit } from 'react-icons/fa';
 
 export default function Profile() {
   const { state } = useLocation();
@@ -8,9 +9,25 @@ export default function Profile() {
   return (
     <div className='main'>
       <div className='profile-info'>
-        <div>{state.user.local} {state.user.classNumber}반 {state.user.name}</div>
-        <div>{state.user.studentId}</div>
-        <Link to="edit" state={{user: state.user}}>편집</Link>
+        <div className='profile-info-row'>
+          <div>{state.user.local}</div>
+          <div>{state.user.classNumber}반</div>
+          <div>{state.user.name}</div>
+        </div>
+        <div className='profile-info-row'>
+          <div>{state.user.studentId}</div>
+          <div>
+            <Link
+              className='profile-edit-link'
+              to="edit"
+              state={{
+                user: state.user
+              }}
+            >
+              <FaUserEdit />
+            </Link>
+          </div>
+        </div>
       </div>
       <Outlet />
     </div>
