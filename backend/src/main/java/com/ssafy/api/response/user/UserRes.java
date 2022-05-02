904c7368fe96654360af0f0d616365cc78777c82
Byeongwan <quddhs4694@naver.com>
2022-05-02 10:33:20 +0900

    FIX:사용자별 조회/사용자 조회 PK 정보 추가

--- backend/src/main/java/com/ssafy/api/response/user/UserRes.java
+++ backend/src/main/java/com/ssafy/api/response/user/UserRes.java
@@ -16,44 +16,48 @@ import java.time.LocalDate;
 @Setter
 @ApiModel("UserResponse")
 public class UserRes{
-	@ApiModelProperty(name="User ID")
+	@ApiModelProperty(name = "User PK", example = "1")
+	Long id;
+
+	@ApiModelProperty(name="User ID", example = "ssafy")
 	String userId;
 
-	@ApiModelProperty(name = "User Nickname")
+	@ApiModelProperty(name = "User Nickname", example = "떵유")
 	String nickname;
 
-	@ApiModelProperty(name = "User Name")
+	@ApiModelProperty(name = "User Name", example = "김싸피")
 	String name;
 
-	@ApiModelProperty(name = "User Gender")
+	@ApiModelProperty(name = "User Gender", example = "true")
 	Boolean gender;
 
-	@ApiModelProperty(name = "User Email")
+	@ApiModelProperty(name = "User Email", example = "ssafy1@ssafy.com")
 	String email;
 
-	@ApiModelProperty(name = "User Admin")
+	@ApiModelProperty(name = "User Admin", example = "2")
 	Long admin;
 
-	@ApiModelProperty(name = "User Status Total Mileage")
+	@ApiModelProperty(name = "User Status Total Mileage", example = "1000000")
 	Long totalMileage;
 
-	@ApiModelProperty(name = "User Status Remain Mileage")
+	@ApiModelProperty(name = "User Status Remain Mileage", example = "100000")
 	Long remainMileage;
 
-	@ApiModelProperty(name = "User StudentStatus Student Id")
+	@ApiModelProperty(name = "User StudentStatus Student Id", example = "0610000")
 	String studentId;
 
-	@ApiModelProperty(name = "User StudentStatus Class Number")
+	@ApiModelProperty(name = "User StudentStatus Class Number", example = "2")
 	String classNumber;
 
-	@ApiModelProperty(name = "User StudentStatus Team Code")
+	@ApiModelProperty(name = "User StudentStatus Team Code", example = "C200")
 	String teamCode;
 
-	@ApiModelProperty(name = "User StudentStatus Local")
+	@ApiModelProperty(name = "User StudentStatus Local", example = "Gwangju")
 	String local;
 
 	public static UserRes of(User user, Status status, StudentStatus studentStatus) {
 		UserRes res = new UserRes();
+		res.setId(user.getId());
 		res.setUserId(user.getUserId());
 		res.setNickname(user.getNickname());
 		res.setName(user.getName());
