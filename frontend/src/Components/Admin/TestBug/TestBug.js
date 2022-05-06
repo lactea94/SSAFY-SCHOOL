91f06b043c472be0bc90a4380828c9fa58174535
dannyp0930 <dannyp0930@gmail.com>
2022-05-06 17:54:19 +0900

    FEAT: 버그 리포트 호출

--- frontend/src/Components/Admin/TestBug/TestBug.js
+++ frontend/src/Components/Admin/TestBug/TestBug.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import useGetList from "../../../Hooks/useGetList";
 import DateFormat from "../../../Utils/DateFormat";
 import Pagination from "../../Pagination/Pagination";
 import Search from "../../Search/Search";
@@ -8,7 +9,6 @@ import "./css/TestBug.css";
 export function TestBug() {
   const [ searchCategory, setSearchCategory ] = useState('content');
   const [ searchText, setSearchText ] = useState('');
-  const [ logs, setLogs ] = useState([]);
   const [ filteredLogs, setFilteredLogs ] = useState([]);
   const [ limit, setLimit ] = useState(10);
   const [ page, setPage ] = useState(1);
@@ -24,23 +24,7 @@ export function TestBug() {
 
 
   // 버그 리포트 호출
-  useEffect(() => {
-    setLogs([
-      { id: 0, userId: 1, content: "내용1", createdDate: "2022-04-19 15:30:30" },
-      { id: 1, userId: 7, content: "내용2", createdDate: "2022-04-18 15:30:30" },
-      { id: 2, userId: 4, content: "내용3", createdDate: "2022-04-17 15:30:30" },
-      { id: 3, userId: 2, content: "내용4", createdDate: "2022-04-16 15:30:30" },
-      { id: 4, userId: 11, content: "내용5", createdDate: "2022-04-15 15:30:30" },
-      { id: 5, userId: 9, content: "내용6", createdDate: "2022-04-14 15:30:30" },
-      { id: 6, userId: 7, content: "내용7", createdDate: "2022-04-13 15:30:30" },
-      { id: 7, userId: 6, content: "내용8", createdDate: "2022-04-12 15:30:30" },
-      { id: 8, userId: 4, content: "내용9", createdDate: "2022-04-11 15:30:30" },
-      { id: 9, userId: 5, content: "내용10", createdDate: "2022-04-10 15:30:30" },
-      { id: 10, userId: 2, content: "내용11", createdDate: "2022-04-09 15:30:30" },
-      { id: 11, userId: 3, content: "내용12", createdDate: "2022-04-08 15:30:30" },
-      { id: 12, userId: 1, content: "내용13", createdDate: "2022-04-07 15:30:30" },
-    ])
-  }, []);
+  const logs = useGetList('/report');
 
   // 검색 필터링
   useEffect(() => {
