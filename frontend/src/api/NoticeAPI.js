4903789c5c914ca2785cc0f87ed335fa8dad22e1
dannyp0930 <dannyp0930@gmail.com>
2022-05-06 15:00:35 +0900

    FEAT: 공지사항, 버그리포트 API

--- frontend/src/api/NoticeAPI.js
+++ frontend/src/api/NoticeAPI.js
@@ -5,11 +5,31 @@ const API = authInstance();
 // 공지사항 작성
 export async function CreateNotice(title, content) {
   try {
-    const res = await API.post('/notice', {
+    await API.post('/notice', {
       title: title,
       content: content,
     });
-    console.log(res)
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// 공지사항 수정
+export async function UpdateNotice(noticeId, title, content) {
+  try {
+    await API.put(`/notice/${noticeId}`, {
+      title: title,
+      content: content,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// 공지사항 삭제
+export async function DeleteNotice(noticeId) {
+  try {
+    await API.delete(`/notice/${noticeId}`);
   } catch (error) {
     console.log(error);
   }
