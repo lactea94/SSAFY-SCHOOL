594a4ef4aaa2b171838019fdc870aac0f4df0bc2
김동유 <ehddb2252@gmail.com>
2022-05-08 23:02:39 +0900

    UPDATE: change payload

--- nodeJs/src/server.js
+++ nodeJs/src/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const { stringify } = require('querystring');
 var app = express();
 app.use(express.static(__dirname + '/public'));
@@ -14,34 +15,26 @@ var io = require('socket.io')(http, {
 });
 
 io.on("connection", (socket) => {
-  socket.on("nickname", ({DataString, DataByte}) => { socket["nickname"] = DataString; console.log(DataString)});
-  socket.on("join", ({DataString, DataByte}) => { socket.join(DataString); });
-  socket.on("video", ({DataString, DataByte}) => { socket.to(DataString).emit("video", {DataString: socket.nickname, DataByte}); console.log(1);});
-  socket.on('OnReceiveData', function (data){
-        switch(data.EmitType)
-        {
-            //emit type: all;
-            case 0: io.emit('OnReceiveData', { DataString: data.DataString, DataByte: data.DataByte }); break;
-            //emit type: server;
-            case 1: io.to(serverID).emit('OnReceiveData', { DataString: data.DataString, DataByte: data.DataByte }); break;
-            //emit type: others;
-            case 2: socket.broadcast.emit('OnReceiveData', { DataString: data.DataString, DataByte: data.DataByte }); break;
-        }
+  socket.on("join", ({room, nickname}) => { 
+    socket.join(room); 
+    socket["nickname"] = nickname;
+  });
+  socket.on("leave", ({room}) => { socket.leave(room); });
+  socket.on("msg", ({msg, nickname, room}) => { socket.to(room).emit("msg", {msg, nickname}) });
+  socket.on("video", ({room, byteData}) => { socket.to(room).emit("video", {nickname: socket.nickname, byteData}); });
+  socket.on("mic", ({room, byteData}) => { socket.to(room).emit("mic", {nickname: socket.nickname, byteData}); console.log("hi")});
 
-        console.log('cannot find any active server');
+  socket.on("putLocation", ({stringData}) => { // stringData = {url, token, vector}
+    fetch(stringData[0], {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Authorization": "Bearer "+ stringData[1],
+      },
+      body: JSON.stringify({ location : stringData[2] }),
     });
-  // socket.on("RPC", ({room, variables, func}) => { socket.to(room).emit("RPC", {variables, func}); });
-  // socket.on("putLocation", ({url, token, vector}) => {
-  //   fetch(url, {
-  //     method: "PUT",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       "Access-Control-Allow-Origin": "*",
-  //       "Authorization": "Bearer "+ token,
-  //     },
-  //     body: JSON.stringify({ location : vector }),
-  //   });
-  // });
+  });
 });
 
 http.listen(3000, function(){ console.log('listening on *:3000');});
