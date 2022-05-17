7e93ebee401e43e19d31e8feef03c20dd38add14
Byeongwan <quddhs4694@naver.com>
2022-05-17 11:23:12 +0900

    FIX:CheckIn/Out 로직 수정

--- backend/src/main/java/com/ssafy/db/repository/CheckInRepository.java
+++ backend/src/main/java/com/ssafy/db/repository/CheckInRepository.java
@@ -5,12 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CheckInRepository extends JpaRepository<CheckIn, Long> {
     Optional<CheckIn> findById(Long id);
+    Optional<CheckIn> findByCreatedDateAndUserId(LocalDate createdDate, Long userId);
     List<CheckIn> findAllByUserId(Long userId);
 
     @Query(value = "select * from check_in", nativeQuery = true)
